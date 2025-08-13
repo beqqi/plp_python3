f6a83d633b939de8790d3f47d3c1d43306761678
beqqi <100137642+beqqi@users.noreply.github.com>
2025-08-13 18:33:10 +0300

    week 3 python assignment initail commit

---
+++ discount.py
@@ -0,0 +1,17 @@
+def calculate_discount(price, discount_percent):
+    """
+    Calculate the discounted price based on the original price and discount percentage.
+
+    :param price: Original price of the item
+    :param discount_percent: Discount percentage to apply
+    :return: Discounted price
+    """
+    if discount_percent >= 0.2:
+        # Apply a discount only if the discount percentage is 20% or more
+        return price * (1 - discount_percent)
+    return price
+
+
+price = float(input("Enter the original price: "))
+discount_percent = float(input("Enter the discount percent in decimals: "))
+print(f"The price of this item is {calculate_discount(price, discount_percent)}")
